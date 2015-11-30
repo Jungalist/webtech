66d0de591e4c6a839b0d1bb9da761b42fff92b5f
Jungalist <smolor@hotmail.co.uk>
2015-11-30 01:20:51 +0000

    Made the jumbotron and messed around with navbar

--- project/class/contact.php
+++ project/class/contact.php
@@ -1,32 +1,25 @@
 <?php
-/**
- * A class that contains code to implement a contact page
- *
- * @author Lindsay Marshall <lindsay.marshall@ncl.ac.uk>
- * @copyright 2012-2013 Newcastle University
- *
- */
+
+require_once '/vendor/autoload.php';
+
     class Contact extends Siteaction
     {
-/**
- * Handle contact operations /contact/xxxx
- *
- * @param object	$context	The context object for the site
- *
- * @return string	A template name
- */
+
         public function handle($context)
         {
+			$loader	= new Twig_Loader_Filesystem('twigs');
+			$twig = new Twig_Environment($loader);
+			
+			
             if (($msg = $context->postpar('message', '')) != '')
             { # there is a post
                 mail(Config::SYSADMIN, $context->postpar('subject', 'No Subject'), $msg);
-                $context->local()->addval('done', TRUE);
             }
-            return 'contact.twig';
-        }
+                   
 		
-		echo $twig->render('homepage.twig', array(
-			'hi' => 'Hello World',
-		));
+			echo $twig->render('contact.twig', array(
+				'hi' => 'Hello World',
+			));
+		}
     }
 ?>
--- project/class/upload.php
+++ project/class/upload.php
@@ -12,25 +12,61 @@ require_once '/vendor/autoload.php';
 			$twig = new Twig_Environment($loader);
 			
 			$success = False;
-			
+			//$test = 0;
 			//formHandling($context, $success);
-			$id = 0;
-			if ( ($authorName = $context->postpar('author', '')) != ''){
+			//Add comma separated, multiple authors
+			
+			
+			//Make getting vars from form into function?
+			if (($authorName = $context->postpar('author', '')) != '' and 
+				($iName = $context->postpar('iName', '')) != '' and
+				($country = $context->postpar('country', '')) != '' and 
+				($pubName = $context->postpar('pubName', '')) != ''){
+				//Add author if not exists
+				
+				//$test = $this->ifexists($authorName);				
 				$newAuthor = R::dispense('author');
-				//$newAuthor->authorId = '71';
 				$newAuthor->name = $authorName;
 				//$newAuthor->main_subject = postpar('subject', '');
-				$id = R::store($newAuthor);
+				$authID = R::store($newAuthor);
+				
+				
+				//Add intitution if not exists
+				$newInsti = R::dispense('institution');
+				$newInsti->name = $iName;
+				$newInsti->country = $country;
+				$instiID = R::store($newInsti);
+				
+				//Check name of pub, if exists ask what to do
+				//Add Publication
+				$newPub = R::dispense('publication');
+				$newPub->name = $pubName;
+				$newPub->authorID = $authID;
+				$newPub->institutionID = $instiID;
+				// TODO actually upload the file and store url
+				$newPub->url = '';
+				R::store($newPub);
+				
 				$success = True;
 			}
 			
 			echo $twig->render('upload.twig', array(
-				'success' => ($success) ? 'Success' : 'Sorry, try again',
-				'id' => $id
+				//Use session cookies to not show if form not submitted or form validation to show, 'Sorry try again'
+				'success' => ($success) ? 'Success' : ''
+				//,'test' => $test
 			)); 
 			
 			
 		}
+		
+		/*function ifExists($authorName){
+			$a = R::find('author', 'author_name = :author', array(
+				':author' => $authorName
+			));
+			
+			return $a;
+		}*/
+		
 		/*
 		public function formHandling($context, $success){
 			if ( ($authorName = $context->postpar('author', '')) != ''){
